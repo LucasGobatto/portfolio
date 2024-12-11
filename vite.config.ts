c05611081927e853253f90b5bdd0fe88dab552c9
LucasGobatto <lucas.bisaio@live.com>
2024-12-11 01:15:36 -0300

    version 1.0.4

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   build: {
     outDir: 'docs',
     assetsDir: '',
-    cssMinify: true,
     emptyOutDir: false,
     cssCodeSplit: false,
     commonjsOptions: { include: ['node_modules/**'] },
